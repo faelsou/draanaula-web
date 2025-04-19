e2e44b6fce2ac9d0c6216d5c61fa8c6aa05cd2ef
faelsou <rafadsouz@gmail.com>
2025-04-19 10:32:56 -0300

    ajustes certs lts1

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,30 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser'; // Usando a importação correta
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  }
-);
+export default {
+  parser: tsParser, // Configurando o parser para o TypeScript
+  extends: [
+    js.configs.recommended, // Configurações recomendadas do ESLint
+    'plugin:@typescript-eslint/recommended', // Configurações recomendadas do @typescript-eslint
+  ],
+  files: ['**/*.{ts,tsx}'],
+  ignorePatterns: ['dist'], // Ignora a pasta dist
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
